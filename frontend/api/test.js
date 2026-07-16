0975cfebc83983ca5ca75539ae464cf9e327dc4f
SHPL <saipoojita29@gmail.com>
2026-07-16 15:26:02 +0530

    Update test.js

--- frontend/api/test.js
+++ frontend/api/test.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
   res.status(200).json({
-    success: true,
-    message: "Backend working"
+    mid: process.env.ICICI_MID,
+    aggId: process.env.ICICI_AGG_ID,
+    hasKey: !!process.env.ICICI_KEY,
+    returnUrl: process.env.ICICI_RETURN_URL
   });
 }
